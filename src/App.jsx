267a79965b3cc6f2901c5f3e511ca4e47f1fedb2
chaehaeun <chae_3210@naver.com>
2023-05-18 22:10:41 +0900

    [add] 283. Link로 페이지들 간에 이동하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from './pages/Home';
-import Products from './pages/Products';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
 
-const router = createBrowserRouter(
-  { path : '/', element : <Home/> },
-  { path : '/products', element : <Products/>}
-);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <h1>My Home Page</h1>
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products">the list of products</Link>
+      </p>
+    </>
   );
 };
 
