cc9e46e8c68e0266b6f37d5555b08bd11289b1d8
chaehaeun <chae_3210@naver.com>
2023-05-19 23:10:17 +0900

    [add] 288. 동적 라우트 정의하고 사용하기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>My Home Page</h1>
       <p>
         Go to <Link to="/products">the list of products</Link>
       </p>
+      <button type="button" onClick={navigateHandler}>
+        Navigate
+      </button>
     </>
   );
 };
