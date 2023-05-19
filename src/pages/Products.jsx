305929148d9c8dc65abc05762524f0a5f40bc85c
chaehaeun <chae_3210@naver.com>
2023-05-19 23:32:08 +0900

    [add] 288. 동적 라우트 정의하고 사용하기

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Products = () => {
-  return <h1>My Products Page</h1>;
+  return (
+    <>
+      <h1>My Products Page</h1>;
+      <ul>
+        <li>product 1</li>
+        <li>product 2</li>
+        <li>product 3</li>
+      </ul>
+    </>
+  );
 };
 
 export default Products;
