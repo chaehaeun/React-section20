e536048927cf186dec3d0440b65af5dbe1104665
chaehaeun <chae_3210@naver.com>
2023-05-21 01:02:51 +0900

    [add] 297. loader() 데이터의 다양한 활용법 (useNavigation)

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import MainNavigation from "../components/MainNavigation";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 const RootLayout = () => {
+  // const navigation = useNavigation();
+  // useNavigation : 현재 라우트의 네비게이션 객체를 가져오는 훅
+  // 현재 전환이 진행 중인지, 데이터를 불러오는 중인지 등을 알 수 있음
+
   return (
     <>
       <MainNavigation />
       <main>
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
