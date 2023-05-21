60e7eea903c1b19c9fe60189e1275f3045e2af73
chaehaeun <chae_3210@naver.com>
2023-05-21 21:47:43 +0900

    [add] json() 유틸 함수

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,5 +1,5 @@
 import EventsList from "../components/EventsList";
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 // useLoaderData : 가장 가까운 라우트의 loader 데이터를 가져오는 훅
 
 function EventsPage() {
@@ -23,9 +23,13 @@ export async function loader() {
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events!" };
     // throw new Error("Could not fetch events!");
-    throw new Response(JSON.stringify({ message: "Could not fetch events!" }), {
-      status: 500,
-    });
+    // throw new Response(JSON.stringify({ message: "Could not fetch events!" }), {
+    //   status: 500,
+    // });
+    return json({ message: "Could not fetch events!" }, { status: 500 });
+    // 번거롭게 Response 객체를 만들지 않고 json()을 쓰면
+    // json 형태로 바로 리턴해줌
+    // json()은 json 형식의 데이터가 포함된 Response 객체를 생성하는 react router dom 함수
   } else {
     return response;
   }
