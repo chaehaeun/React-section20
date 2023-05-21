a824d7e39ffc02773c57e4e3decc3ffee96c13c2
chaehaeun <chae_3210@naver.com>
2023-05-21 23:34:13 +0900

    [add] 309. action() 함수 사용하기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRoot from "./pages/EventsRoot";
@@ -50,6 +50,7 @@ const router = createBrowserRouter([
             // 이렇게 쓰면 new를 eventId로 인식해서 EventDetailPage로 가버릴 것이라 생각할 수 있지만
             // 리액트라우터는 똑똑이기 때문에 new 페이지로 갈 것임ㅇㅇ 신경 안 써도 됨
             element: <NewEventPage />,
+            action: newEventAction,
           },
         ],
       },
